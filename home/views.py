dbd3c50e65bdfc32c4d8ba6713c1e021ed697678
thaohinnn <thaohinn1002@gmail.com>
2024-03-23 20:04:08 +0700

    Make title page dynamic

--- home/views.py
+++ home/views.py
@@ -2,56 +2,70 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    page_name =  {"page_name" : "Hanja Hero"}
+    return render(request, 'home.html', page_name)
 
 
 def login(request):
-    return render(request, 'login.html')
+    page_name =  {"page_name" : "Log In"}
+    return render(request, 'login.html', page_name)
 
 
 def signup(request):
-    return render(request, 'register.html')
+    page_name =  {"page_name" : "Sign Up"}
+    return render(request, 'register.html', page_name)
 
 
 def userterms(request):
-    return render(request, 'userterms.html')
+    page_name =  {"page_name" : "User Terms"}
+    return render(request, 'userterms.html', page_name)
 
 
 def aboutus(request):
-    return render(request, 'aboutus.html')
+    page_name =  {"page_name" : "About Us"}
+    return render(request, 'aboutus.html', page_name)
 
 
 def abouttheTOPIKexam(request):
-    return render(request, 'abouttheTOPIKexam.html')
+    page_name =  {"page_name" : "About TOPIK"}
+    return render(request, 'abouttheTOPIKexam.html', page_name)
 
 
 def TOPIKexamdates(request):
-    return render(request, 'TOPIKexamdates.html')
+    page_name =  {"page_name" : "TOPIK Exam Dates"}
+    return render(request, 'TOPIKexamdates.html', page_name)
 
 
 def mocktestslibrary(request):
-    return render(request, 'mocktestslibrary.html')
+    page_name =  {"page_name" : "Mock Tests Library"}
+    return render(request, 'mocktestslibrary.html', page_name)
 
 
 def practicetestslibrary(request):
-    return render(request, 'practicetestslibrary.html')
+    page_name =  {"page_name" : "Practice Tests Library"}
+    return render(request, 'practicetestslibrary.html', page_name)
 
 
 def topik1mocklibrary(request):
-    return render(request, 'topik1mocklibrary.html')
+    page_name =  {"page_name" : "TOPIK I Mock Tests"}
+    return render(request, 'topik1mocklibrary.html', page_name)
 
 
 def topik2mocklibrary(request):
-    return render(request, 'topik2mocklibrary.html')
+    page_name =  {"page_name" : "TOPIK II Mock Tests"}
+    return render(request, 'topik2mocklibrary.html', page_name)
 
 
 def topik1practicelibrary(request):
-    return render(request, 'topik1practicelibrary.html')
+    page_name =  {"page_name" : "TOPIK I Practice Tests"}
+    return render(request, 'topik1practicelibrary.html', page_name)
 
 
 def test(request):
-    return render(request, 'test.html')
+    page_name =  {"page_name" : "TOPIK II Practice Tests"}
+    return render(request, 'test.html', page_name)
 
 
 def passwordrecover(request):
-    return render(request, 'recover.html')
+    page_name =  {"page_name" : "Forgot Password?"}
+    return render(request, 'recover.html', page_name)
