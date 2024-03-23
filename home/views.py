86d78c5324df947b80f839f40d43fc5d2f680926
thaohinnn <thaohinn1002@gmail.com>
2024-03-23 20:29:11 +0700

    rename files to standard snake_case

--- home/views.py
+++ home/views.py
@@ -6,59 +6,59 @@ def home(request):
     return render(request, 'home.html', page_name)
 
 
-def login(request):
+def log_in(request):
     page_name =  {"page_name" : "Log In"}
     return render(request, 'login.html', page_name)
 
 
-def signup(request):
+def sign_up(request):
     page_name =  {"page_name" : "Sign Up"}
     return render(request, 'register.html', page_name)
 
 
-def userterms(request):
+def user_terms(request):
     page_name =  {"page_name" : "User Terms"}
-    return render(request, 'userterms.html', page_name)
+    return render(request, 'user_terms.html', page_name)
 
 
-def aboutus(request):
+def about_us(request):
     page_name =  {"page_name" : "About Us"}
-    return render(request, 'aboutus.html', page_name)
+    return render(request, 'about_us.html', page_name)
 
 
-def abouttheTOPIKexam(request):
+def about_the_topik_exam(request):
     page_name =  {"page_name" : "About TOPIK"}
-    return render(request, 'abouttheTOPIKexam.html', page_name)
+    return render(request, 'about_the_topik.html', page_name)
 
 
-def TOPIKexamdates(request):
+def topik_exam_dates(request):
     page_name =  {"page_name" : "TOPIK Exam Dates"}
-    return render(request, 'TOPIKexamdates.html', page_name)
+    return render(request, 'topik_exam_dates.html', page_name)
 
 
-def mocktestslibrary(request):
+def mock_tests_library(request):
     page_name =  {"page_name" : "Mock Tests Library"}
-    return render(request, 'mocktestslibrary.html', page_name)
+    return render(request, 'mock_tests_library.html', page_name)
 
 
-def practicetestslibrary(request):
+def practice_tests_library(request):
     page_name =  {"page_name" : "Practice Tests Library"}
-    return render(request, 'practicetestslibrary.html', page_name)
+    return render(request, 'practice_tests_library.html', page_name)
 
 
-def topik1mocklibrary(request):
+def topik1_mock_library(request):
     page_name =  {"page_name" : "TOPIK I Mock Tests"}
-    return render(request, 'topik1mocklibrary.html', page_name)
+    return render(request, 'topik1_mock_library.html', page_name)
 
 
-def topik2mocklibrary(request):
+def topik2_mock_library(request):
     page_name =  {"page_name" : "TOPIK II Mock Tests"}
-    return render(request, 'topik2mocklibrary.html', page_name)
+    return render(request, 'topik2_mock_library.html', page_name)
 
 
-def topik1practicelibrary(request):
+def topik1_practice_library(request):
     page_name =  {"page_name" : "TOPIK I Practice Tests"}
-    return render(request, 'topik1practicelibrary.html', page_name)
+    return render(request, 'topik1_practice_library.html', page_name)
 
 
 def test(request):
@@ -66,6 +66,6 @@ def test(request):
     return render(request, 'test.html', page_name)
 
 
-def passwordrecover(request):
+def password_recover(request):
     page_name =  {"page_name" : "Forgot Password?"}
     return render(request, 'recover.html', page_name)
