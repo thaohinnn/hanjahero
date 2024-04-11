4a70d1c45806f52f3209759f5461bf246410734a
thaohinnn <thaohinn1002@gmail.com>
2024-04-11 11:27:35 +0700

    add few fields for models TestHistory and UserTestResult

--- home/models/test_history.py
+++ home/models/test_history.py
@@ -7,5 +7,7 @@ class TestHistory(models.Model):
     test_history_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     exam_name = models.IntegerField(default=0)
+    skill_name = models.IntegerField(default=0)
+    format_name = models.JSONField(null=True)
     test_date = models.DateField()
     score = models.FloatField()
