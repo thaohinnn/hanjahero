86459c5fb03b1882fef9de3f3605f278948cb096
thaohinn <thaohinn1002@gmail.com>
2024-05-16 18:27:23 +0700

    add CSRF_TRUSTED_ORIGINS

--- hanjahero/settings.py
+++ hanjahero/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
-
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
 
 # Application definition
 
